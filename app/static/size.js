1f17a720ae9d5254d2ce35ac54710c057789d80a
Ngan Vu <ngan.vu@yale.edu>
2018-12-02 05:01:54 +0000

    add brush size slider

--- app/static/size.js
+++ app/static/size.js
@@ -2,18 +2,22 @@
 window.brush_size = 8;
 
 function changeBrushSize(brush_size) {
-	switch(brush_size.id) {
-		case "small":
-			window.brush_size = 5;
-			break;
-		case "medium":
-			window.brush_size = 10;
-			break;
-		case "large":
-			window.brush_size = 15;
-			break;
+	if (brush_size.id) {
+		switch(brush_size.id) {
+			case "small":
+				window.brush_size = 5;
+				break;
+			case "medium":
+				window.brush_size = 10;
+				break;
+			case "large":
+				window.brush_size = 15;
+				break;
+		}
+	} else {
+		window.brush_size = brush_size / 2;
 	}
 };
 
-changeBrushSize({id: "small"});
+changeBrushSize(30);
 window.changeBrushSize = changeBrushSize;
